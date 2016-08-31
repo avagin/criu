748700804f7421c7f1c83460cf9abedd5bcb6801
Andrew Vagin <avagin@virtuozzo.com>
2016-09-01 01:55:25 +0300

    files: add a function to reopen fd as an unused fd
    This function finds an unused fd and reopens a specified fd into it.
    
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/files.c
+++ criu/files.c
@@ -112,12 +112,9 @@ struct fdinfo_list_entry *find_used_fd(struct list_head *head, int fd)
 	return NULL;
 }
 
-void collect_task_fd(struct fdinfo_list_entry *new_fle, struct rst_info *ri)
+static void collect_used_fd(struct fdinfo_list_entry *new_fle, struct rst_info *ri)
 {
 	struct fdinfo_list_entry *fle;
-
-	/* fles in fds list are disordered */
-	list_add_tail(&new_fle->ps_list, &ri->fds);
 
 	/* fles in used list are ordered by fd */
 	list_for_each_entry(fle, &ri->used, used_list) {
@@ -126,6 +123,38 @@ void collect_task_fd(struct fdinfo_list_entry *new_fle, struct rst_info *ri)
 	}
 
 	list_add_tail(&new_fle->used_list, &fle->used_list);
+}
+
+void collect_task_fd(struct fdinfo_list_entry *new_fle, struct rst_info *ri)
+{
+	/* fles in fds list are disordered */
+	list_add_tail(&new_fle->ps_list, &ri->fds);
+
+	return collect_used_fd(new_fle, ri);
+}
+
+int reopen_as_unused_fd(int fd, struct rst_info *rst)
+{
+	struct {
+		struct fdinfo_list_entry fle;
+		FdinfoEntry		 fe;
+	} *fd_entry;
+	int tfd;
+
+	tfd = find_unused_fd(&rst->used, fd);
+	if (tfd < 0)
+		return -1;
+
+	if (reopen_fd_as(tfd, fd))
+		return -1;
+
+	fd_entry = xmalloc(sizeof(*fd_entry));
+	fd_entry->fle.fe = &fd_entry->fe;
+	fd_entry->fe.fd = tfd;
+
+	collect_used_fd(&fd_entry->fle, rst);
+
+	return tfd;
 }
 
 unsigned int find_unused_fd(struct list_head *head, int hint_fd)
