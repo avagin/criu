7e5cddaaeb2c41bde3992baebb00336e02ae3c3b
Andrei Vagin <avagin@virtuozzo.com>
2016-11-13 09:03:32 +0300

    zdtm: check that sequence numbers for tcp sockets
    They have to be the same on each iteration.

--- test/zdtm.py
+++ test/zdtm.py
@@ -1019,6 +1019,7 @@ def cr(cr_api, test, opts):
 			sbs('pre-restore')
 			try_run_hook(test, ["--pre-restore"])
 			cr_api.restore()
+                        os.environ["ZDTM_IMG_DIR"] = cr_api.logs()
 			try_run_hook(test, ["--post-restore"])
 			sbs('post-restore')
 
