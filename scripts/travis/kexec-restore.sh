a20a0f3f941fda1248b3f82a3ad39d1de494b222
Andrei Vagin <avagin@openvz.org>
2016-12-05 18:34:53 -0800

    xxx

--- scripts/travis/kexec-restore.sh
+++ scripts/travis/kexec-restore.sh
@@ -7,6 +7,9 @@ sleep 10
 cd $1
 mkfifo $2
 chmod 0600 $2
-./criu/criu restore -D /imgs -o restore.log -j --tcp-established --ext-unix-sk -v4 -l
+./criu/criu restore -D /imgs -o restore.log -j --tcp-established --ext-unix-sk -v4 -l -d
 echo $?
 touch /rebooted
+while :; do
+sleep 1000
+done
