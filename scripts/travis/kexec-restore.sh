f544a359e5cee5a70fe7075585fd136869146ed9
root <avagin@openvz.org>
2016-12-06 01:13:15 +0000

    xxx
    Conflicts:
    	.travis.yml
    	scripts/travis/travis-tests

---
+++ scripts/travis/kexec-restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+exec &> /travis.log
+
+set -x
+sleep 10
+cd $1
+mkfifo $2
+chmod 0600 $2
+./criu/criu restore -D /imgs -o restore.log -j --tcp-established --ext-unix-sk -v4 -l
+echo $?
+touch /rebooted
---
+++ scripts/travis/kexec.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+ppid=""
+pid=$$
+while :; do
+	p=`cat /proc/$pid/status | grep PPid | awk '{ print $2 }'`
+	if [ "$p" -eq 1 ]; then
+		break;
+	fi
+	ppid=$pid
+	pid=$p
+	echo $pid
+done
+
+ls -l /boot/
+
+setsid bash -c "setsid ./scripts/travis/kexec-dump.sh $ppid < /dev/null &> /travis.log &"
+while :; do
+	sleep 1
+	uname -a
+	uptime
+	ps axf
+	if [ -f /rebooted ]; then
+		break;
+	fi
+done
