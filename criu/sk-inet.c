d44c8743a4c694ab645a399837618762bc65a7e9
Andrei Vagin <avagin@virtuozzo.com>
2016-12-14 01:45:41 +0300

    xxx

--- criu/sk-inet.c
+++ criu/sk-inet.c
@@ -628,11 +628,6 @@ static int open_inet_sk(struct file_desc *d)
 	if (restore_opt(sk, SOL_SOCKET, SO_REUSEADDR, &yes))
 		goto err;
 
-	if (ie->src_port) {
-		if (inet_bind(sk, ii))
-			goto err;
-	}
-
 	if (tcp_connection(ie)) {
 		if (!opts.tcp_established_ok) {
 			pr_err("Connected TCP socket in image\n");
@@ -647,7 +642,11 @@ static int open_inet_sk(struct file_desc *d)
 		mutex_unlock(&ii->port->reuseaddr_lock);
 
 		goto done;
+	} else if (ie->src_port) {
+		if (inet_bind(sk, ii))
+			goto err;
 	}
+
 
 	/*
 	 * Listen sockets are easiest ones -- simply
