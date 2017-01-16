d19ef88d490aee7818a117048c5c91574f6a0833
Andrei Vagin <avagin@virtuozzo.com>
2017-01-16 09:44:12 +0300

    soccr: add one more test to check libsoccr_save/libsoccr_restore
    [root@fc24 test]# valgrind ./tcp-conn
    ==31562== Memcheck, a memory error detector
    ==31562== Copyright (C) 2002-2015, and GNU GPL'd, by Julian Seward et al.
    ==31562== Using Valgrind-3.11.0 and LibVEX; rerun with -h for copyright info
    ==31562== Command: ./tcp-conn
    ==31562==
    ==31562== Warning: noted but unhandled ioctl 0x894b with no size/direction hints.
    ==31562==    This could cause spurious value errors to appear.
    ==31562==    See README_MISSING_SYSCALL_OR_IOCTL for guidance on writing a proper wrapper.
    Debug: 	Setting 1 queue seq to 2004100269
    Debug: 	Setting 2 queue seq to 2651007575
    Debug: 	Restoring TCP options
    Debug: 		Will turn SAK on
    Debug: 		Will set snd_wscale to 7
    Debug: 		Will set rcv_wscale to 7
    Debug: 		Will turn timestamps on
    Debug: Will set mss clamp to 65495
    Debug: 	Restoring TCP 1 queue data 11 bytes
    Error: Failed to turn off repair mode on socket: Bad file descriptor
    ==31562==
    ==31562== HEAP SUMMARY:
    ==31562==     in use at exit: 0 bytes in 0 blocks
    ==31562==   total heap usage: 4 allocs, 4 frees, 1,116 bytes allocated
    ==31562==
    ==31562== All heap blocks were freed -- no leaks are possible
    ==31562==
    ==31562== For counts of detected and suppressed errors, rerun with: -v
    ==31562== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)

--- soccr/test/tcp-conn.c
+++ soccr/test/tcp-conn.c
@@ -2,20 +2,38 @@
 #include <arpa/inet.h>  /* for srvaddr_in and inet_ntoa() */
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "../soccr.h"
+#include <stdarg.h>
 
 #define pr_perror(fmt, ...) printf(fmt ": %m\n", ##__VA_ARGS__)
 
-union sockaddr_inet {
-        struct sockaddr_in v4;
-        struct sockaddr_in6 v6; 
+enum {
+	TCP_NO_QUEUE,
+	TCP_RECV_QUEUE,
+	TCP_SEND_QUEUE,
+	TCP_QUEUES_NR,
 };
+static void pr_printf(unsigned int level, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	va_end(args);
+}
 
 int main()
 {
-	union sockaddr_inet addr, dst;
-	int srv, sock, clnt;
-	int ret;
+	union libsoccr_addr addr, dst;
+	int srv, sock, clnt, rst;
+	int ret, dsize;
 	socklen_t dst_let;
+	struct libsoccr_sk_data data = {};
+	struct libsoccr_sk *so, *so_rst;
+	char buf[11] = "0123456789", *queue;
+
+	libsoccr_set_log(10, pr_printf);
 
 	memset(&addr,0,sizeof(addr));
 
@@ -69,9 +87,80 @@ int main()
 	}
 
 	dst_let = sizeof(dst);
-	sock = accept(srv, (struct sockaddr *) &dst, sizeof(dst));
+	sock = accept(srv, (struct sockaddr *) &dst, &dst_let);
 	if (sock < 0) {
 		pr_perror("accept");
+		return 1;
+	}
+
+	if (write(clnt, &buf, sizeof(buf)) != sizeof(buf)) {
+		pr_perror("write");
+		return 1;
+	}
+
+	/* Start testing */
+	dst_let = sizeof(addr);
+	if (getsockname(sock, (struct sockaddr *) &addr, &dst_let)) {
+		pr_perror("connect");
+		return 1;
+	}
+	dst_let = sizeof(addr);
+	if (getpeername(sock, (struct sockaddr *) &dst, &dst_let)) {
+		pr_perror("connect");
+		return 1;
+	}
+
+
+	so = libsoccr_pause(sock);
+
+	dsize = libsoccr_save(so, &data, sizeof(data));
+	if (dsize < 0) {
+		pr_perror("libsoccr_save");
+		return 1;
+	}
+
+#ifndef test_ipv6
+	rst = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+#else
+	rst = socket(af_inet6, SOCK_STREAM, IPPROTO_TCP);
+#endif
+	if (rst == -1) {
+		pr_perror("socket() failed");
+		return -1;
+	}
+	close(sock);
+
+	so_rst = libsoccr_pause(rst);
+	libsoccr_set_addr(so_rst, 1, &addr, 0);
+	libsoccr_set_addr(so_rst, 0, &dst, 0);
+
+	queue = libsoccr_get_queue_bytes(so, TCP_RECV_QUEUE, SOCCR_MEM_EXCL);
+	libsoccr_set_queue_bytes(so_rst, TCP_RECV_QUEUE, queue, SOCCR_MEM_EXCL);
+	queue = libsoccr_get_queue_bytes(so, TCP_SEND_QUEUE, SOCCR_MEM_EXCL);
+	libsoccr_set_queue_bytes(so_rst, TCP_SEND_QUEUE, queue, SOCCR_MEM_EXCL);
+
+	ret = libsoccr_restore(so_rst, &data, dsize);
+	if (ret) {
+		pr_perror("libsoccr_restore: %d", ret);
+		return 1;
+	}
+
+	libsoccr_resume(so_rst);
+	libsoccr_resume(so);
+
+	if (read(rst, &buf, sizeof(buf)) != sizeof(buf)) {
+		pr_perror("read");
+		return 1;
+	}
+
+	if (write(rst, &buf, sizeof(buf)) != sizeof(buf)) {
+		pr_perror("write");
+		return 1;
+	}
+	shutdown(rst, SHUT_WR);
+
+	if (read(clnt, &buf, sizeof(buf)) != sizeof(buf)) {
+		pr_perror("read");
 		return 1;
 	}
 
