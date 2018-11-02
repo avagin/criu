e7a2e6e3f5f818c434a4ff875191e77656020b91
Andrei Vagin <avagin@gmail.com>
2018-11-02 06:43:57 +0300

    xxx

--- scripts/travis/docker-test.sh
+++ scripts/travis/docker-test.sh
@@ -12,12 +12,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable" test
+   stable test"
 
 
-apt-get update -qq
+apt-get update -y
 
-apt-get install -qq docker-ce
+apt-get install -y docker-ce
 
 cat > /etc/docker/daemon.json <<EOF
 {
