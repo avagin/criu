ef442bfc12c1a1cc0ab36f9b986d2f24cdf93c37
AnorexicAtticusFinch <47782857+AnorexicAtticusFinch@users.noreply.github.com>
2020-06-11 19:45:57 +0530

    test 4

--- criu/files-reg.c
+++ criu/files-reg.c
@@ -1424,7 +1424,7 @@ static int get_build_id(const int fd, const struct stat *fd_status,
 	size = note_header->n_descsz;
 	note_header = (Elf_ptr(Nhdr) *) ((size_t) note_header + sizeof(Elf_ptr(Nhdr)) + note_header->n_namesz);
 	note_header_end = (Elf_ptr(Nhdr) *) (file_header_end - size);
-	if (note_header <= (Elf_ptr(Nhdr) *) file_header) || note_header > note_header_end) {
+	if (note_header <= (Elf_ptr(Nhdr) *) file_header || note_header > note_header_end) {
 		munmap(file_header, fd_status->st_size);
 		return -1;
 	}
