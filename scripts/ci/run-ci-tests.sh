64b616efe31fdc9b2bd5cabdba3cde0c9d8a15a2
Andrei Vagin <avagin@gmail.com>
2020-12-23 10:16:49 +0300

    debug

--- scripts/ci/run-ci-tests.sh
+++ scripts/ci/run-ci-tests.sh
@@ -212,6 +212,8 @@ if [ "${STREAM_TEST}" = "1" ]; then
 	exit 0
 fi
 
+./test/zdtm.py run -t zdtm/transition/thread-bomb -f h --keep-going --report report --freezecg zdtm:f --norst
+
 # shellcheck disable=SC2086
 ./test/zdtm.py run -a -p 2 --keep-going $ZDTM_OPTS
 
